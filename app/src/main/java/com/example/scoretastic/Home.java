83b8ba79ab2367ae1c4668269730b2122749cc8f
SHAMMAS <shammasfarooq7@gmail.com>
2020-01-17 19:11:50 +0500

    MAPS AND LIST HIDE SHOW

--- app/src/main/java/com/example/scoretastic/Home.java
+++ app/src/main/java/com/example/scoretastic/Home.java
@@ -3,17 +3,32 @@ package com.example.scoretastic;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Home extends Fragment {
+public class Home extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mMap;
+    View j;
 
 
     public Home() {
@@ -25,7 +40,48 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        final View v =  inflater.inflate(R.layout.fragment_home, container, false);
+        Button btMap = v.findViewById(R.id.btMap);
+        Button btList = v.findViewById(R.id.btList);
+        j = v.findViewById(R.id.map);
+        final RecyclerView recyclerView = v.findViewById(R.id.recyclerView);
+
+
+
+        btMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                recyclerView.setVisibility(View.GONE);
+                j.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recyclerView.setVisibility(View.VISIBLE);
+                j.setVisibility(View.GONE);
+            }
+        });
+
+        return v;
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap =googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }

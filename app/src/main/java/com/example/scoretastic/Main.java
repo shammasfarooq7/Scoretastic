d8aa10f6ce0680e40ab95b8c520db2d23ca3e3a8
HP 15 <huzi4398@gmail.com>
2020-01-17 19:08:35 +0500

    change password settings

--- app/src/main/java/com/example/scoretastic/Main.java
+++ app/src/main/java/com/example/scoretastic/Main.java
@@ -1,15 +1,20 @@
 package com.example.scoretastic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class Main extends AppCompatActivity {
+
+
+    private Toolbar toolbar1;
 
     Button home,createEvent,myEvents,profile;
     FrameLayout frameLayout;
@@ -23,6 +28,8 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        toolbar1 = findViewById(R.id.toolbar1);
         home = findViewById(R.id.home);
         createEvent = findViewById(R.id.create);
         myEvents = findViewById(R.id.myevents);
--- app/src/main/java/com/example/scoretastic/Profile.java
+++ app/src/main/java/com/example/scoretastic/Profile.java
@@ -1,19 +1,34 @@
 package com.example.scoretastic;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Profile extends Fragment {
+public class Profile extends Fragment implements View.OnClickListener {
+
+
+    Button btSettings ,btchgPassword,btFaqs,btAboutUs;
+
+    ImageView ivprofpic, ivchgpic;
+    //TextView tvSettings, tvChgPass;
+    LinearLayout llbt;
 
 
     public Profile() {
@@ -24,8 +39,99 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        initializeView(view);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+
+
+
+        return view;
+
+
+
+
+
     }
 
+    private void initializeView(View view) {
+
+
+        btSettings = view.findViewById(R.id.btSettings);
+        btchgPassword = view.findViewById(R.id.btchgPassword);
+        btFaqs = view.findViewById(R.id.btFaqs);
+        btAboutUs = view.findViewById(R.id.btAboutUs);
+
+
+        btSettings = (Button) view.findViewById(R.id.btSettings);
+        btSettings.setOnClickListener(this);
+
+
+
+        btchgPassword = (Button) view.findViewById(R.id.btchgPassword);
+        btSettings.setOnClickListener(this);
+
+
+        btSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSettings();
+
+            }
+        });
+
+        btchgPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openchangePassword();
+
+            }
+        });
+
+
+
+
+
+
+
+
+        //LLset = view.findViewById(R.id.LLset);
+        //LLchgPass = view.findViewById(R.id.LLchgPass);
+
+        ivprofpic = view.findViewById(R.id.ivprofpic);
+        ivchgpic = view.findViewById(R.id.ivchgpic);
+
+        //tvChgPass = view.findViewById(R.id.tvChgPass);
+        //tvSettings = view.findViewById(R.id.tvSettings);
+    }
+
+
+
+     public void openSettings(){
+
+        Intent intent = new Intent(getActivity(),Settings.class);
+
+         startActivity(intent);
+
+
+
+     }
+
+    public void openchangePassword(){
+
+        Intent intent = new Intent(getActivity(),ChangePassword.class);
+        startActivity(intent);
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }

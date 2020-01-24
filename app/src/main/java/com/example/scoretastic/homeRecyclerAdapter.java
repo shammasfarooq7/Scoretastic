0208e04009aa728973800c66fb251f49c88bfff0
HP 15 <huzi4398@gmail.com>
2020-01-24 18:45:32 +0500

    create event java functionalities

---
+++ app/src/main/java/com/example/scoretastic/homeRecyclerAdapter.java
@@ -0,0 +1,76 @@
+package com.example.scoretastic;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class homeRecyclerAdapter extends RecyclerView.Adapter<homeRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<recycler> event;
+    ItemClicked activity;
+
+    public interface  ItemClicked{
+        void onItemClicked(int index);
+    }
+
+    public homeRecyclerAdapter(Context context, ArrayList<recycler> list){
+        event = list;
+        activity = (ItemClicked) context;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvSports;
+        TextView tvLocation;
+        EditText etTime;
+        EditText etDate;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvSports = itemView.findViewById(R.id.tvSports);
+            tvLocation = itemView.findViewById(R.id.tvLocation);
+            etTime = itemView.findViewById(R.id.etTime);
+            etDate = itemView.findViewById(R.id.etDate);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view, parent,false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.itemView.setTag(event.get(position));
+        holder.tvSports.setText(event.get(position).sports);
+        holder.tvLocation.setText(event.get(position).location);
+        holder.etDate.setText((CharSequence) event.get(position).date);
+        holder.etTime.setText((CharSequence) event.get(position).time);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return event.size();
+    }
+
+}

20c1196d7d44511f1324d3bdf8044bc9c3d95a83
SHAMMAS <shammasfarooq7@gmail.com>
2020-06-12 18:42:25 +0500

    12 June

---
+++ app/src/main/java/com/example/scoretastic/TeamMatchDetails.java
@@ -0,0 +1,14 @@
+package com.example.scoretastic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamMatchDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_match_details);
+    }
+}
